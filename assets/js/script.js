e55dbee0e3c665a7d66c7a37c691965753f65e6b
Christian Jones <c529j466@yahoo.com>
2022-10-07 02:12:11 -0500

    first API call using jquery and adding city and date to html

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+var cityName = "";
+var lat = "";
+var lon = "";
+
+  //function calling the weather API based on the users entered city
+  function weatherInfo() {
+   var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&lang=en&appid=7fbf7fa35159c759fb6f2423ce36d49c";
+
+   $.ajax({
+       url: queryURL,
+       method: "GET"
+   }).then(function (response) {
+       // sets the lat and long variables to the cities latitude and longitude
+       lat = response.coord.lat;
+       lon = response.coord.lon;
+
+       // Adds the users chosen city name to the html doc
+       $("#city").text(response.name);
+       // Adds the date 
+       $("#date").text(moment.unix(response.dt).format("dddd, MM/DD/YYYY"));
+                   
+       // Uses local storage to save the users selected city name
+       localStorage.setItem("cityname", response.name);
+       
+       // Using the latitude and longitude recieved from the response to use in the detailed OneCall API call
+       detailedWeatherCall(lat,lon);
+
+   })
+}
+
